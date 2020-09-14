5d533a6f359122b97a01cdef29bbeff3bf9d9764
Jason SooHoo <jtsoohoo@comcast.net>
2020-09-13 20:07:55 -0700

    Created Views File. Regenerated Secret Key. Secret Key hidden behind env file.

--- wolfnet/wolfnet/settings.py
+++ wolfnet/wolfnet/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import netifaces
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_y^1)*f%ml^e%qy-x02i7eg&n-pdojttidbt7d59gvldk)2!d4'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
